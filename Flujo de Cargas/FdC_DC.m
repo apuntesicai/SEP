5dadf8cadfb11967393ef676625515fac1547be4
nachosanz48 <ignaciosanzsoriano@gmail.com>
2017-05-06 11:51:40 +0200

    Rename FdC_DC_1.m to FdC_DC.m

--- Flujo de Cargas/FdC_DC.m
+++ Flujo de Cargas/FdC_DC.m
@@ -1,7 +1,7 @@
 % TRABAJO DE SEP TEMA4: FLUJO DE CARGAS
 % Fecha: 27/04/2016
-% Autores: IGNACIO SANZ, GUILLERMO MAIRAL, JAVIER MIÑARRO,
-% PEDRO PARDO, IGNACIO PASTOR, SANTIAGO LÓPEZ
+% Autores: IGNACIO SANZ, GUILLERMO MAIRAL, JAVIER MIÃ‘ARRO,
+% PEDRO PARDO, IGNACIO PASTOR, SANTIAGO LÃ“PEZ
 
 clear all
 format short
@@ -51,9 +51,9 @@ Piny = [
 	Pe3 ;
 	];
 
-% Ángulos de las tensiones
+% Ãngulos de las tensiones
 vecTH = invYbusDC_mismP*Piny;
-vecTH = [  vecTH ; 0.0 ]; % Añadir el del slack
+vecTH = [  vecTH ; 0.0 ]; % AÃ±adir el del slack
 TH1 = vecTH(1);
 TH2 = vecTH(2);
 TH3 = vecTH(3);
@@ -63,62 +63,62 @@ disp('Vector de angulos(grados)')
 vecTH = [ vecTH ]*180/pi
 vecTH = vecTH*pi/180;
 
-%% Análisis de contingencias
-% Atención: con invYbusDC_mismP, puesto que posicion no corresponde con nudo
+%% AnÃ¡lisis de contingencias
+% AtenciÃ³n: con invYbusDC_mismP, puesto que posicion no corresponde con nudo
 
-%% 1. Línea 1-2
+%% 1. LÃ­nea 1-2
 % Flujo inicial
 f12_0 = (TH1-TH2)/x12;
 
 % Potencia ficticia (+ en 1, - en 2)
 incP_ctg12 = f12_0/( 1 - (invYbusDC_mismP(1,1) + invYbusDC_mismP(2,2) - invYbusDC_mismP(1,2) - invYbusDC_mismP(2,1) )/x12); % los elementos correspondientes al nudo 1 son nulos
 incTH_ctg12 = invYbusDC_mismP*[ incP_ctg12; -incP_ctg12 ; 0.0 ]; 
-vecTH_ctg12 = vecTH + [  incTH_ctg12 ; 0.0 ]; % Añadir el del slack
+vecTH_ctg12 = vecTH + [  incTH_ctg12 ; 0.0 ]; % AÃ±adir el del slack
 
-%% 2.Línea 2-4
+%% 2.LÃ­nea 2-4
 % Flujo inicial
 f24_0 = (TH2-TH4)/x24;
 
 % Potencia ficticia (+ en 2, - en 4)
 incP_ctg24 = f24_0/( 1 - ( invYbusDC_mismP(2,2) + 0.0 - 0.0 - 0.0 )/x24); 
 incTH_ctg24 = invYbusDC_mismP*[ 0.0 ; incP_ctg24 ; 0.0 ]; 
-vecTH_ctg24 = vecTH + [  incTH_ctg24 ; 0.0]; % Añadir el del slack
+vecTH_ctg24 = vecTH + [  incTH_ctg24 ; 0.0]; % AÃ±adir el del slack
 
-%% 3. Línea 2-3 
+%% 3. LÃ­nea 2-3 
 % Flujo inicial
 f23_0 = 2*(TH2-TH3)/x23;
 
 % Potencia ficticia (+ en 1, - en 4)
 incP_ctg23 = f23_0/( 1 - (invYbusDC_mismP(2,2) + invYbusDC_mismP(3,3) - invYbusDC_mismP(2,3) - invYbusDC_mismP(2,3) )/x14); % los elementos correspondientes al nudo 1 son nulos
 incTH_ctg23 = invYbusDC_mismP*[ 0.0 ; incP_ctg23; -incP_ctg23 ]; 
-vecTH_ctg23 = vecTH + [  incTH_ctg23 ; 0.0 ]; % Añadir el del slack
+vecTH_ctg23 = vecTH + [  incTH_ctg23 ; 0.0 ]; % AÃ±adir el del slack
 
-% Nota: Las dos líneas 1-4 son iguales, por tanto si se pierde sólo 1 de las dos,
+% Nota: Las dos lÃ­neas 1-4 son iguales, por tanto si se pierde sÃ³lo 1 de las dos,
 % es indiferente cual se haya perdido
 
-%% 4. Línea 1-4 (si se pierde sólo 1 de las dos)
+%% 4. LÃ­nea 1-4 (si se pierde sÃ³lo 1 de las dos)
 % Flujo inicial
 f14_0 = (TH1-TH4)/x14;
 
 % Potencia ficticia (+ en 1, - en 4)
 incP_ctg14 = f14_0/( 1 - (invYbusDC_mismP(1,1) + 0.0 - 0.0 - 0.0 )/x14); % los elementos correspondientes al nudo 1 son nulos
 incTH_ctg14 = invYbusDC_mismP*[ incP_ctg14; 0.0 ; 0.0 ]; 
-vecTH_ctg14 = vecTH + [  incTH_ctg14 ; 0.0 ]; % Añadir el del slack
+vecTH_ctg14 = vecTH + [  incTH_ctg14 ; 0.0 ]; % AÃ±adir el del slack
 
-%% 5. Línea 1-4 (si se pierden las dos líneas)
+%% 5. LÃ­nea 1-4 (si se pierden las dos lÃ­neas)
 % Flujo inicial
 f14_0_bis = 2*(TH1-TH4)/x14;
 
 % Potencia ficticia (+ en 1, - en 4)
 incP_ctg14_bis = f14_0/( 1 - (invYbusDC_mismP(1,1) + 0.0 - 0.0 - 0.0 )/2*x14); % los elementos correspondientes al nudo 1 son nulos
 incTH_ctg14_bis = invYbusDC_mismP*[ +incP_ctg14_bis; 0.0 ; 0.0 ]; 
-vecTH_ctg14_bis = vecTH + [  incTH_ctg14_bis ; 0.0 ]; % Añadir el del slack
+vecTH_ctg14_bis = vecTH + [  incTH_ctg14_bis ; 0.0 ]; % AÃ±adir el del slack
 
 %% RESULTADOS
 echo off
 disp('   ');
 disp('Resumen Solucion');
-disp('   TH [º] ')
+disp('   TH [Âº] ')
 disp('n    base    linea 12  linea 23  linea 24  linea 14  linea 14x2 ')
 for ii=1:4
 	disp(sprintf('%1d %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f ',ii,vecTH(ii)*180/pi,vecTH_ctg12(ii)*180/pi,vecTH_ctg23(ii)*180/pi,vecTH_ctg24(ii)*180/pi,vecTH_ctg14(ii)*180/pi,vecTH_ctg14_bis(ii)*180/pi))
