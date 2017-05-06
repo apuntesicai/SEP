55834fa3f1ec7dfd164c8f0ee663648d5c1c3d7c
nachosanz48 <ignaciosanzsoriano@gmail.com>
2017-05-06 11:53:11 +0200

    Rename MatrizYbus.m to FdC_Ybus.m

--- Flujo de Cargas/FdC_Ybus.m
+++ Flujo de Cargas/FdC_Ybus.m
@@ -1,7 +1,7 @@
 % TRABAJO DE SEP TEMA4: FLUJO DE CARGAS
 % Fecha: 27/04/2016
-% Autores: IGNACIO SANZ, GUILLERMO MAIRAL, JAVIER MI—ARRO,
-% PEDRO PARDO, IGNACIO PASTOR, SANTIAGO L”PEZ
+% Autores: IGNACIO SANZ, GUILLERMO MAIRAL, JAVIER MI√ëARRO,
+% PEDRO PARDO, IGNACIO PASTOR, SANTIAGO L√ìPEZ
 
 clear all
 format short
@@ -12,16 +12,16 @@ echo on
 % EC 1
 % Los datos de una red de 4 nudos son los siguientes:
 
-% INI	FIN	DescripciÛn
+% INI	FIN	Descripci√≥n
 % 2	3	TRANSFORMADOR: 200 MVA, 410/20kV; ucc = 13%
-% 1	4	LÕNEA: 400kV; zs = 0.03+j0.31 p.u., yp = j0.87 p.u.
-% 1	4	LÕNEA: 400kV; zs = 0.03+j0.31 p.u., yp = j0.87 p.u.
-% 1	2	LÕNEA: 400kV; zs = 0.02+j0.23 p.u., yp = j0.52 p.u.
-% 2	4	LÕNEA: 400kV; zs = 0.01+j0.19 p.u., yp = j0.41 p.u.
+% 1	4	L√çNEA: 400kV; zs = 0.03+j0.31 p.u., yp = j0.87 p.u.
+% 1	4	L√çNEA: 400kV; zs = 0.03+j0.31 p.u., yp = j0.87 p.u.
+% 1	2	L√çNEA: 400kV; zs = 0.02+j0.23 p.u., yp = j0.52 p.u.
+% 2	4	L√çNEA: 400kV; zs = 0.01+j0.19 p.u., yp = j0.41 p.u.
 
 % Los datos de los transformadores son relativos a sus propias bases.
-% Los datos de las lÌneas son relativos a las bases del sistema.
-% Obtener la matriz de admitancias nodales Ybus, considerando bases trif·sicas del sistema de 400/220kV y 100 MVA.
+% Los datos de las l√≠neas son relativos a las bases del sistema.
+% Obtener la matriz de admitancias nodales Ybus, considerando bases trif√°sicas del sistema de 400/220kV y 100 MVA.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CONSTANTES
 J = sqrt(-1) ;
@@ -35,8 +35,8 @@ Zbase = Ubase./Ibase
 % YBUS
 YBUS = zeros(4,4)
 
-%% LÌneas
-% LÌnea 1 - 4
+%% L√≠neas
+% L√≠nea 1 - 4
 zs_14 = 0.03 + J*0.31;
 yp_14 = J*0.87;
 YBUS(1,1) = YBUS(1,1) + 1/zs_14 + yp_14/2 ;
@@ -45,7 +45,7 @@ YBUS(4,1) = YBUS(4,1) - 1/zs_14 ;
 YBUS(4,4) = YBUS(4,4) + 1/zs_14 + yp_14/2 ; 
 YBUS
 
-% LÌnea 1 - 4 BIS
+% L√≠nea 1 - 4 BIS
 zs_14bis = 0.03 + J*0.31;
 yp_14bis = J*0.87;
 YBUS(1,1) = YBUS(1,1) + 1/zs_14 + yp_14/2 ;
@@ -54,7 +54,7 @@ YBUS(4,1) = YBUS(4,1) - 1/zs_14bis ;
 YBUS(4,4) = YBUS(4,4) + 1/zs_14bis + yp_14bis/2 ; 
 YBUS
 
-% LÌnea 1 - 2
+% L√≠nea 1 - 2
 zs_12 = 0.02 + J*0.23;
 yp_12 = J*0.52;
 YBUS(1,1) = YBUS(1,1) + 1/zs_12 + yp_12/2 ;
@@ -63,7 +63,7 @@ YBUS(2,1) = YBUS(2,1) - 1/zs_12 ;
 YBUS(2,2) = YBUS(2,2) + 1/zs_12 + yp_12/2 ; 
 YBUS
 
-% LÌnea 2 - 4
+% L√≠nea 2 - 4
 zs_24 = 0.01 + J*0.19;
 yp_24 = J*0.41;
 YBUS(2,2) = YBUS(2,2) + 1/zs_24 + yp_24/2 ;
